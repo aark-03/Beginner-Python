a95f8eb476e294d6dce87ea06ade2b61ccd2ab5a
aark-03 <aarushiksd@gmail.com>
2026-08-02 22:27:16 -0400

    Add .format to print statement

--- Nickname Generator.py
+++ Nickname Generator.py
@@ -5,4 +5,4 @@ name = input('Enter your first name: ').strip()
 
 nickname = name[0]+name[-1]+name[0]+name[-1]
 nickname = nickname.lower().capitalize()
-print('Your nickname is:',nickname)
+print('Your nickname is: {}!'.format(nickname))
