0bb6ab0d714d9feff99452c950f60e13aa7147bb
aark-03 <aarushiksd@gmail.com>
2026-08-02 22:25:58 -0400

    Initial commit

---
+++ Nickname Generator.py
@@ -0,0 +1,8 @@
+#Nickname Generator
+
+print('Create your nickname!')
+name = input('Enter your first name: ').strip()
+
+nickname = name[0]+name[-1]+name[0]+name[-1]
+nickname = nickname.lower().capitalize()
+print('Your nickname is:',nickname)
