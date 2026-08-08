a4ee739b7a23b6e5fa2c5f0978a6d6af80c28f11
aark-03 <aarushiksd@gmail.com>
2026-08-08 01:24:07 -0400

    Added to file

---
+++ String Frame.py
@@ -0,0 +1,12 @@
+#String frame
+
+#Creates function to calculate borders and sides of frame
+def frame_string(a):
+    length = len(a) + 6
+    borders = '*'*length
+    sides = '*  '+a+'  *'
+    print(borders,sides,borders,sep='\n')
+
+#Gets user input and passes through function
+a = input('Enter string: ').strip()
+print(frame_string(a))
